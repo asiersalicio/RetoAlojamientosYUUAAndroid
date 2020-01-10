2a1d63e146e0d78bd4ac7c791dd72b145aa79eee
asiersalicio <salicioasier99@gmail.com>
2020-01-10 14:13:13 +0100

    Google login

--- app/src/main/java/com/yuua/alojamientosyuua/Login.java
+++ app/src/main/java/com/yuua/alojamientosyuua/Login.java
@@ -1,15 +1,92 @@
 package com.yuua.alojamientosyuua;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient apiClient;
+    private SignInButton signInButton;
+    private static int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inizializar();
+
         getSupportActionBar().hide();
     }
+
+    private void inizializar() {
+        GoogleSignInOptions gso =  new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        apiClient=new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+        signInButton=(SignInButton)findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(apiClient);
+                startActivityForResult(intent,RC_SIGN_IN);
+            }
+        });
+    }
+
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+    private void handleSignInResult(GoogleSignInResult result){
+        if(result.isSuccess()){
+            gotoProfile(result);
+        }else{
+            Toast.makeText(getApplicationContext(),"Sign in cancel",Toast.LENGTH_LONG).show();
+        }
+    }
+    private void gotoProfile(GoogleSignInResult result){
+        GoogleSignInAccount account=result.getSignInAccount();
+
+
+        Toast.makeText(this, "INICIADO SESION CON EXITO", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, account.getEmail(), Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this, "Error de conexion!", Toast.LENGTH_SHORT).show();
+        Log.e("GoogleSignIn", "OnConnectionFailed: " + connectionResult);
+    }
+
+
 }
