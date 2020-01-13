e132be659f88bfd945e8d952aca97830b4927b51
asiersalicio <salicioasier99@gmail.com>
2020-01-13 12:28:34 +0100

    actiobar

--- app/src/main/java/com/yuua/alojamientosyuua/activitys/Base.java
+++ app/src/main/java/com/yuua/alojamientosyuua/activitys/Base.java
@@ -1,10 +1,14 @@
 package com.yuua.alojamientosyuua.activitys;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
@@ -12,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -30,6 +35,7 @@ public class Base extends AppCompatActivity {
     private FragmentReservas fragment_reservas;
     private FragmentUsuario fragment_usuario;
     public static Context contexto;
+    private Toolbar toolbar;
 
     public static LinearLayoutManager llm;
 
@@ -46,11 +52,16 @@ public class Base extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        aplicarAnimaciones();
         establecerDatosUsuario();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        getSupportActionBar().setElevation(0);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbar_base, menu);
 
@@ -61,8 +72,8 @@ public class Base extends AppCompatActivity {
     public void Inicializar()
     {
         contexto=this;
+        toolbar=findViewById(R.id.searchbar);
         bottomNavigationView=findViewById(R.id.bottomnavigationview);
-
         llm = new LinearLayoutManager(contexto);
         fragmentInicio = new FragmentInicio(contexto);
         getSupportFragmentManager().beginTransaction().replace(R.id.framelayoutbase,fragmentInicio).commit();
@@ -107,6 +118,24 @@ public class Base extends AppCompatActivity {
             bottomNavigationView.getMenu().getItem(2).setTitle(user.getNombreUsuario());
         }
     }
+
+    public void aplicarAnimaciones()
+    {
+        ValueAnimator anim = ValueAnimator.ofInt(toolbar.getMeasuredHeight(), 100);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int val = (Integer) valueAnimator.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = toolbar.getLayoutParams();
+                layoutParams.height = val;
+                toolbar.setLayoutParams(layoutParams);
+            }
+        });
+        anim.setDuration(3000);
+        anim.start();
+    }
+
+
 
 
 
