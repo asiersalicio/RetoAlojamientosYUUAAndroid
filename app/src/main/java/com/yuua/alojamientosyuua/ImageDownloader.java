a3a650d09aa81fb2cca970816432d426d9c6c4f0
asiersalicio <salicioasier99@gmail.com>
2020-01-20 12:45:31 +0100

    descargador de imagenes

--- app/src/main/java/com/yuua/alojamientosyuua/ImageDownloader.java
+++ app/src/main/java/com/yuua/alojamientosyuua/ImageDownloader.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.os.FileUtils;
 import android.util.Log;
 
 import androidx.core.app.ActivityCompat;
@@ -17,12 +18,19 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -53,24 +61,48 @@ public class ImageDownloader extends Thread {
         }
 
 
-        parseJSON(resul);
+
 
 
         return resul;
     }
 
-    private void parseJSON(String resul) {
+    private String parseJSON(String resul) {
+            String resultado = resul;
+
         Log.println(Log.DEBUG,"JSON",resul);
 
-            resul=resul.substring( resul.indexOf("\"media\":")+9);
-            resul=resul.substring(0,resul.indexOf("\",\""));
+        resultado=resultado.substring( resul.indexOf("\"media\":")+9);
+        Log.println(Log.DEBUG,"JSON",resultado);
+        resultado=resultado.substring(0,resul.indexOf(","));
 
-            resul=resul.replace("\\","");
+        resultado=resultado.replace("\\","");
 
-            System.out.println(resul);
+            System.out.println(resultado);
+
+            return resultado;
 
 
+    }
 
+    private void downloadImage(String imageUrl)
+    {
+        try {
+            System.out.println("URL imagen " +imageUrl);
+            URL url = new URL(imageUrl);
+            InputStream is = url.openStream();
+            OutputStream os = new FileOutputStream(Environment.getDataDirectory().getAbsolutePath() + "/prueba.jpg");
+            System.out.println(Environment.getDataDirectory().getAbsolutePath());
+            byte[] b = new byte[2048];
+            int length;
+
+            while ((length = is.read(b)) != -1) {
+                os.write(b, 0, length);
+            }
+
+            is.close();
+            os.close();
+        }catch (IOException ex){ex.printStackTrace();}
     }
 
     @Override
@@ -82,15 +114,18 @@ public class ImageDownloader extends Thread {
             in = new BufferedReader(new InputStreamReader(url.openStream()));
             while ((line = in.readLine()) != null) {
 
-
                 all+=line;
-
 
             }
             in.close();
+
+            String parseado;
+            parseado=parseJSON(all);
+            downloadImage(parseado);
+
+
         } catch (IOException ex) {
         }
-        setResultado(all);
         setFinalizado(true);
     }
 
