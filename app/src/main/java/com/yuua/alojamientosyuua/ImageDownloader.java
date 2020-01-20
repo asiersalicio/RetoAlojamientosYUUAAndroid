c79eac6e4fdde44f9230cb0964d4909673fae8af
asiersalicio <salicioasier99@gmail.com>
2020-01-20 13:27:10 +0100

    Descarga de imagen arreglada

--- app/src/main/java/com/yuua/alojamientosyuua/ImageDownloader.java
+++ app/src/main/java/com/yuua/alojamientosyuua/ImageDownloader.java
@@ -1,53 +1,31 @@
 package com.yuua.alojamientosyuua;
 
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.os.Environment;
-import android.os.FileUtils;
 import android.util.Log;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class ImageDownloader extends Thread {
 
     private URL url;
-    private boolean finalizado=false;
+    private boolean finalizado = false;
     private String resul;
     private BufferedWriter writer;
 
     private Context context;
 
 
-    public ImageDownloader(Context context)
-    {
-        this.context=context;
+    public ImageDownloader(Context context) {
+        this.context = context;
     }
 
 
@@ -56,39 +34,25 @@ public class ImageDownloader extends Thread {
         url = new URL(URL);
         start();
 
-        while (!finalizado){
+        while (!finalizado) {
 
         }
-
-
-
 
 
         return resul;
     }
 
     private String parseJSON(String resul) {
-            String resultado = resul;
-
-        Log.println(Log.DEBUG,"JSON",resul);
-
-        resultado=resultado.substring( resul.indexOf("\"media\":")+9);
-        Log.println(Log.DEBUG,"JSON",resultado);
-        resultado=resultado.substring(0,resul.indexOf(","));
-
-        resultado=resultado.replace("\\","");
-
-            System.out.println(resultado);
-
-            return resultado;
-
-
+        String resultado = resul;
+        resultado = resultado.substring(resul.indexOf("\"media\":") + 9);
+        resultado = resultado.substring(0, resultado.indexOf(","));
+        resultado = resultado.replace("\\", "");
+        return resultado;
     }
 
-    private void downloadImage(String imageUrl)
-    {
+    private void downloadImage(String imageUrl) {
         try {
-            System.out.println("URL imagen " +imageUrl);
+            System.out.println("URL imagen " + imageUrl);
             URL url = new URL(imageUrl);
             InputStream is = url.openStream();
             OutputStream os = new FileOutputStream(Environment.getDataDirectory().getAbsolutePath() + "/prueba.jpg");
@@ -102,7 +66,9 @@ public class ImageDownloader extends Thread {
 
             is.close();
             os.close();
-        }catch (IOException ex){ex.printStackTrace();}
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
@@ -114,13 +80,13 @@ public class ImageDownloader extends Thread {
             in = new BufferedReader(new InputStreamReader(url.openStream()));
             while ((line = in.readLine()) != null) {
 
-                all+=line;
+                all += line;
 
             }
             in.close();
 
             String parseado;
-            parseado=parseJSON(all);
+            parseado = parseJSON(all);
             downloadImage(parseado);
 
 
@@ -130,18 +96,13 @@ public class ImageDownloader extends Thread {
     }
 
 
-    private void setFinalizado(boolean finalizado)
-    {
-        this.finalizado=finalizado;
+    private void setFinalizado(boolean finalizado) {
+        this.finalizado = finalizado;
     }
 
-    private void setResultado(String string)
-    {
-        resul=string;
+    private void setResultado(String string) {
+        resul = string;
     }
-
-
-
 
 
 }
